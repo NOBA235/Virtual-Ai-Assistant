a5bf817905b19b951e31b65e4436299329886f3e
NOBA235 <nanova556@gmail.com>
2025-10-30 22:12:36 +0530

    Fixed CORS: updated frontend URL to markey1.vercel.app

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "https://virtual-ai-assistant.vercel.app",
+    "https://markey1.vercel.app",,
     "http://localhost:5173" 
   ],
   methods: ["GET", "POST"],
